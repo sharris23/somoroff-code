c45761deb41edc68deab7f90ff8e53790ce711da
sharris23 <sensh23@aol.com>
2017-10-22 20:10:49 -0400

    playlist added
    started playlist adder function

--- playlist.js
+++ playlist.js
@@ -1,3 +1,6 @@
 var playlist = new Array();
 /*button code to be used for each video*/
 /*.widgetcont is what you'll be adding these divs to*/
+function playadd (vid) {
+	playlist[playlist.length]=document.getElementById(vid);
+}
