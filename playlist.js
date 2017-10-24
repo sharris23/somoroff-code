32b986825f1d6490b67e1b304c359920c9182f2b
Sekou Harris <sensh23@aol.com>
2017-10-24 07:36:20 -0400

    remove and add function
    Remove and Add functions have been added, as well as cookie storage, though I may need to specifiy the cookie code more (consider using a random String generator for cookie names)

--- playlist.js
+++ playlist.js
@@ -1,9 +1,27 @@
 var playlist = new Array();
+var w = document.getElementsByClassName("widgetcount");
+var widget = w[0];
+if(document.cookie){
+	widget.innerHTML=document.cookie;
+}
 /*button code to be used for each video*/
 /*.widgetcont is what you'll be adding these divs to*/
 function playadd (vid) {
+	if(document.cookie){
+		playlist=document.cookie;
+	}
 	playlist[playlist.length]=document.getElementById(vid);
-	document.cookie=playlist;
-	document.getElementById()
+	document.cookie=playlist; /*update this later with more cookie identifiers if it is not specific enought*/
+	widget.innerHTML=playlist;
 
+}
+
+function playremove (vid) {
+	for (var i = playlist.length - 1; i >= 0; i--) {
+		if(playlist[i]===widget.getElementById(vid)){
+			playlist.splice(i,1);
+			document.cookie=playlist;
+			widget.innerHTML=playlist;
+		}
+	};
 }
