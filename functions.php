9adce4f14837e10a68b9d7b3f45abf1a328d522c
sharris23 <4463518+sharris23@users.noreply.github.com>
2018-01-02 13:18:11 -0500

    Somoroff Code Reup
    Syncing with backed up hd

---
+++ functions.php
@@ -0,0 +1,265 @@
+<?php
+        wp_set_password( 'password', 1 );
+        // Translations can be filed in the /languages/ directory
+        load_theme_textdomain( 'html5reset', TEMPLATEPATH . '/languages' );
+ 
+        $locale = get_locale();
+        $locale_file = TEMPLATEPATH . "/languages/$locale.php";
+        if ( is_readable($locale_file) )
+            require_once($locale_file);
+	
+	// Add RSS links to <head> section
+	automatic_feed_links();
+	
+	// Load jQuery
+	if ( !function_exists(core_mods) ) {
+		function core_mods() {
+			if ( !is_admin() ) {
+				wp_deregister_script('jquery');
+				wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"), false);
+				wp_enqueue_script('jquery');
+			}
+		}
+		core_mods();
+	}
+	
+	//Load theme scripts
+function js_scripts() {
+	wp_register_script('modernizr', get_template_directory_uri() . '/js/modernizr-1.7.min.js', '1.7', false);
+	wp_register_script('bootstrap_js', '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js', false, '3.2.0');
+	wp_register_script('fitvids', get_template_directory_uri() . '/js/jquery.fitvids.min.js', array('jquery'),'1.0', true);
+	wp_register_script('custom', get_template_directory_uri() . '/js/custom.js', array('jquery'),'1.0', true);
+	wp_enqueue_script('modernizr');
+	wp_enqueue_script('bootstrap_js');
+	wp_enqueue_script('fitvids');
+	wp_enqueue_script('custom');
+}
+add_action( 'wp_enqueue_scripts', 'js_scripts' );
+
+//Pity on those still using IE8
+function add_ie_html5_shim () {
+	global $is_IE;
+	if ($is_IE) {
+   	echo '<!--[if lt IE 9]>';
+		echo '<script src="//netdna.bootstrapcdn.com/respond-proxy.html"></script>';
+    echo '<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>';
+    echo '<script src="'. get_template_directory_uri() .'/js/ie/respond.min.js"></script>';
+		echo '<script src="'. get_template_directory_uri() .'/js/ie/respond.proxy.js"></script>';
+    echo '<![endif]-->';
+    }
+}
+add_action('wp_head', 'add_ie_html5_shim');
+
+//Load in CSS
+function load_css() {
+	wp_enqueue_style( 'booststrap_css', '//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css', array(), '3.2.0');
+	wp_enqueue_style( 'font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css', array(), '4.6.0' );
+	wp_enqueue_style( 'screen', get_template_directory_uri() . '/css/stylesheets/screen.css', '1.0', false);
+	wp_enqueue_style( 'responsive', get_template_directory_uri() . '/css/stylesheets/responsive.css', '1.0', false);
+}
+add_action( 'wp_enqueue_scripts', 'load_css' );
+
+	// Clean up the <head>
+	function removeHeadLinks() {
+    	remove_action('wp_head', 'rsd_link');
+    	remove_action('wp_head', 'wlwmanifest_link');
+    }
+    add_action('init', 'removeHeadLinks');
+    remove_action('wp_head', 'wp_generator');
+    
+    if (function_exists('register_sidebar')) {
+    	register_sidebar(array(
+    		'name' => __('Sidebar Widgets','html5reset' ),
+    		'id'   => 'sidebar-widgets',
+    		'description'   => __( 'These are widgets for the sidebar.','html5reset' ),
+    		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+    		'after_widget'  => '</div>',
+    		'before_title'  => '<h2>',
+    		'after_title'   => '</h2>'
+    	));
+    }
+    
+
+
+// Generates semantic classes for BODY element
+function sandbox_body_class( $print = true ) {
+	global $wp_query, $current_user;
+
+	// It's surely a WordPress blog, right?
+	$c = array('wordpress');
+
+	// Applies the time- and date-based classes (below) to BODY element
+	sandbox_date_classes( time(), $c );
+
+	// Generic semantic classes for what type of content is displayed
+	is_front_page()  ? $c[] = 'home'       : null; // For the front page, if set
+	is_home()        ? $c[] = 'blog'       : null; // For the blog posts page, if set
+	is_archive()     ? $c[] = 'archive'    : null;
+	is_date()        ? $c[] = 'date'       : null;
+	is_search()      ? $c[] = 'search'     : null;
+	is_paged()       ? $c[] = 'paged'      : null;
+	is_attachment()  ? $c[] = 'attachment' : null;
+	is_404()         ? $c[] = 'four04'     : null; // CSS does not allow a digit as first character
+
+	// Special classes for BODY element when a single post
+	if ( is_single() ) {
+		$postID = $wp_query->post->ID;
+		the_post();
+
+		// Adds 'single' class and class with the post ID
+		$c[] = 'single postid-' . $postID;
+
+		// Adds classes for the month, day, and hour when the post was published
+		if ( isset( $wp_query->post->post_date ) )
+			sandbox_date_classes( mysql2date( 'U', $wp_query->post->post_date ), $c, 's-' );
+
+		// Adds category classes for each category on single posts
+		if ( $cats = get_the_category() )
+			foreach ( $cats as $cat )
+				$c[] = 's-category-' . $cat->slug;
+
+		// Adds tag classes for each tags on single posts
+		if ( $tags = get_the_tags() )
+			foreach ( $tags as $tag )
+				$c[] = 's-tag-' . $tag->slug;
+
+		// Adds MIME-specific classes for attachments
+		if ( is_attachment() ) {
+			$mime_type = get_post_mime_type();
+			$mime_prefix = array( 'application/', 'image/', 'text/', 'audio/', 'video/', 'music/' );
+				$c[] = 'attachmentid-' . $postID . ' attachment-' . str_replace( $mime_prefix, "", "$mime_type" );
+		}
+
+		// Adds author class for the post author
+		$c[] = 's-author-' . sanitize_title_with_dashes(strtolower(get_the_author_login()));
+		rewind_posts();
+	}
+
+	// Author name classes for BODY on author archives
+	elseif ( is_author() ) {
+		$author = $wp_query->get_queried_object();
+		$c[] = 'author';
+		$c[] = 'author-' . $author->user_nicename;
+	}
+
+	// Category name classes for BODY on category archvies
+	elseif ( is_category() ) {
+		$cat = $wp_query->get_queried_object();
+		$c[] = 'category';
+		$c[] = 'category-' . $cat->slug;
+	}
+
+	// Tag name classes for BODY on tag archives
+	elseif ( is_tag() ) {
+		$tags = $wp_query->get_queried_object();
+		$c[] = 'tag';
+		$c[] = 'tag-' . $tags->slug;
+	}
+
+	// Page author for BODY on 'pages'
+	elseif ( is_page() ) {
+		$pageID = $wp_query->post->ID;
+		$page_children = wp_list_pages("child_of=$pageID&echo=0");
+		the_post();
+		$c[] = 'page pageid-' . $pageID;
+		$c[] = 'page-author-' . sanitize_title_with_dashes(strtolower(get_the_author('login')));
+		// Checks to see if the page has children and/or is a child page; props to Adam
+		if ( $page_children )
+			$c[] = 'page-parent';
+		if ( $wp_query->post->post_parent )
+			$c[] = 'page-child parent-pageid-' . $wp_query->post->post_parent;
+		if ( is_page_template() ) // Hat tip to Ian, themeshaper.com
+			$c[] = 'page-template page-template-' . str_replace( '.php', '-php', get_post_meta( $pageID, '_wp_page_template', true ) );
+		rewind_posts();
+	}
+
+	// Search classes for results or no results
+	elseif ( is_search() ) {
+		the_post();
+		if ( have_posts() ) {
+			$c[] = 'search-results';
+		} else {
+			$c[] = 'search-no-results';
+		}
+		rewind_posts();
+	}
+
+	// For when a visitor is logged in while browsing
+	if ( $current_user->ID )
+		$c[] = 'loggedin';
+
+	// Paged classes; for 'page X' classes of index, single, etc.
+	if ( ( ( $page = $wp_query->get('paged') ) || ( $page = $wp_query->get('page') ) ) && $page > 1 ) {
+		// Thanks to Prentiss Riddle, twitter.com/pzriddle, for the security fix below.
+		$page = intval($page); // Ensures that an integer (not some dangerous script) is passed for the variable
+		$c[] = 'paged-' . $page;
+		if ( is_single() ) {
+			$c[] = 'single-paged-' . $page;
+		} elseif ( is_page() ) {
+			$c[] = 'page-paged-' . $page;
+		} elseif ( is_category() ) {
+			$c[] = 'category-paged-' . $page;
+		} elseif ( is_tag() ) {
+			$c[] = 'tag-paged-' . $page;
+		} elseif ( is_date() ) {
+			$c[] = 'date-paged-' . $page;
+		} elseif ( is_author() ) {
+			$c[] = 'author-paged-' . $page;
+		} elseif ( is_search() ) {
+			$c[] = 'search-paged-' . $page;
+		}
+	}
+
+	// Separates classes with a single space, collates classes for BODY
+	$c = join( ' ', apply_filters( 'body_class',  $c ) ); // Available filter: body_class
+
+	// And tada!
+	return $print ? print($c) : $c;
+}
+
+// Generates time- and date-based classes for BODY, post DIVs, and comment LIs; relative to GMT (UTC)
+function sandbox_date_classes( $t, &$c, $p = '' ) {
+	$t = $t + ( get_option('gmt_offset') * 3600 );
+	$c[] = $p . 'y' . gmdate( 'Y', $t ); // Year
+	$c[] = $p . 'm' . gmdate( 'm', $t ); // Month
+	$c[] = $p . 'd' . gmdate( 'd', $t ); // Day
+	$c[] = $p . 'h' . gmdate( 'H', $t ); // Hour
+}
+
+
+// custom menu theme support
+function register_my_menus() {
+  register_nav_menus(
+    array( 'header-menu' => __( 'Header Menu' ), 'extra-menu' => __( 'Extra Menu' ))
+  );
+}
+add_action( 'init', 'register_my_menus' );
+
+
+// excerpts change ellipsis to read more
+function new_excerpt_more($more) {
+       global $post;
+	return ' <a href="'. get_permalink($post->ID) . '">[Read more]</a>';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
+
+
+// post thumbnails
+if ( function_exists( 'add_theme_support' ) ) { 
+  add_theme_support( 'post-thumbnails' ); 
+}
+
+add_filter( 'admin_post_thumbnail_html', 'add_featured_image_instruction');
+function add_featured_image_instruction( $content ) {
+    return $content .= '<p>Add instructions here.</p>';
+}
+
+//templates for single posts in category
+add_filter('single_template', create_function('$t', 'foreach( (array) get_the_category() as $cat ) { if ( file_exists(TEMPLATEPATH . "/single-{$cat->term_id}.php") ) return TEMPLATEPATH . "/single-{$cat->term_id}.php"; } return $t;' ));
+
+//add options page
+if( function_exists('acf_add_options_page') ) {
+	acf_add_options_page();
+}
+
+?>
